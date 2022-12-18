7b06ca1ac1a548f94e7fed06aa3a3c41ebc482e1
aniket0131 <aniketchapla@gmail.com>
2022-12-18 16:13:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById("search-btn").addEventListener("click", function () {
+    var my = document.getElementsByClassName("searchhh");
+    if(my.style.cssText=="display:none;"){
+         my.style.cssText=="display:block";
+    }else{
+        my.style.cssText=="display:none";
+    }
+})
