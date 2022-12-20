ee425a169b16e26e750aeede37d7b2fd35ecc7b9
aniket0131 <aniketchapla@gmail.com>
2022-12-20 19:27:28 +0530

    second

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-document.getElementById("search-btn").addEventListener("click", function () {
-    var my = document.getElementsByClassName("searchhh");
-    if(my.style.cssText=="display:none;"){
-         my.style.cssText=="display:block";
-    }else{
-        my.style.cssText=="display:none";
+// document.getElementById("search-btn").addEventListener("click", function () {
+//     var my = document.getElementsByClassName("searchhh");
+//     if(my.style.cssText=="display:none")
+//     {
+//          my.style.cssText=="display:block";
+//     }
+//     else{
+//         my.style.cssText=="display:none";
+//     }
+
+
+function myfunction()
+{
+    var my = document.getElementById("search-box");
+    if(my.style.display == "none")
+    {
+         my.style.display="block";
     }
-})
+    else{
+        my.style.display="none";
+    }
+}
+
+
+function functio()
+{
+    var cart = document.getElementsByClassName("shopping-cart");
+    if(cart.style.display == "none"){
+        cart.style.display = "block";
+
+    }
+    else{
+        cart.style.display = "none";
+    }
+}
